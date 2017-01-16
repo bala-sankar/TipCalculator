fb6ca462f674ea7e0416aaf5633c3655ab6f8c00
Balachandar Sankar <balas@yahoo-inc.com>
2017-01-16 11:59:59 -0800

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Balachandar Sankar on 1/16/17.
+//  Copyright © 2017 Balachandar Sankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Balachandar Sankar on 1/16/17.
+//  Copyright © 2017 Balachandar Sankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
